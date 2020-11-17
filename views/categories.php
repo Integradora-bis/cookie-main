cae677e2951d2af7367cbb69a23c429832cf1727
sofiaGonzaloUtch <71847414+sofiaGonzaloUtch@users.noreply.github.com>
2020-11-16 20:19:09 -0700

    Add files via upload

---
+++ views/categories.php
@@ -0,0 +1,5 @@
+<?php 
+    include_once("../components/header.php");
+    include_once("../components/categories.php");
+    include_once("../components/footer.php");
+?>
---
+++ views/recipe.php
@@ -0,0 +1,5 @@
+<?php 
+    include_once("../components/header.php");
+    include_once("../components/recipe.php");
+    include_once("../components/footer.php");
+?>

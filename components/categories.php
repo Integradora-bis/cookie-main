69085e0ecdfba32cbc52f732d0ff3c4c7dfdb44b
sofiaGonzaloUtch <71847414+sofiaGonzaloUtch@users.noreply.github.com>
2020-11-16 21:40:11 -0700

    Update categories.php

--- components/categories.php
+++ components/categories.php
@@ -6,94 +6,99 @@
  <p>Choose the category that best suits you</p> <br>
  </div>
  
- <div class = "container col-10">
+<div class = "container col-10">
  
-<!-- Categories 1 -->
-
-<div class="row row-cols-1 row-cols-md-2">
-
- <!--Foods-->        
- <div class="col mb-4">
-     <div id ="foods" class="card">
-     <a href="https://www.thispersondoesnotexist.com">
-         <img src="../img/food.jpg" class="card-img-top" alt="Foods" style="width:100% height:auto">
-         <div class="card-body">
-             <h5 class="card-title">Foods</h5>              
-         </div>  
-     </div>
-     </a>     
- </div> 
-
- <!--Desserts-->
- <div class="col mb-4">
-     <div id ="desserts" class="card">
-     <a href="https://www.windows93.net/#!catex">
-         <img src="../img/dessert.jpg" class="card-img-top" alt="dessert" style="width:100% height:auto">
-         <div class="card-body">
-             <h5 class="card-title">Desserts</h5>              
-         </div>  
-     </div>
-     </a>     
- </div> 
-               
-</div>
+ <!-- Categories 1 -->
  
-<!-- Categories 2 -->
-
-<div class="row row-cols-1 row-cols-md-2">
-
- <!--Family-->           
- <div class="col mb-4">
-     <div id ="family" class="card">
-     <a href="https://femurdesign.com/omni/">
-         <img src="../img/family.jpg" class="card-img-top" alt="family" style="width:100% height:auto">
-         <div class="card-body">
-             <h5 class="card-title">For the family</h5>              
-         </div>  
+ <div class="row row-cols-1 row-cols-md-2">
+ 
+         <!--Foods-->        
+         <div class="col mb-4">
+             <div id ="foods" class="card">
+             <a href="https://www.thispersondoesnotexist.com">
+                 <img src="../img/food.jpg" class="card-img-top" alt="Foods" style="width:100% height:auto">
+                 <div class="card-body">
+                     <h5 class="card-title">Foods</h5>              
+                 </div>  
+             </div>
+             </a>     
+         </div> 
+     
+         <!--Desserts-->
+         <div class="col mb-4">
+             <div id ="desserts" class="card">
+             <a href="https://www.windows93.net/#!catex">
+                 <img src="../img/dessert.jpg" class="card-img-top" alt="dessert" style="width:100% height:auto">
+                 <div class="card-body">
+                     <h5 class="card-title">Desserts</h5>              
+                 </div>  
+             </div>
+             </a>     
+         </div> 
+                     
+ </div>
+     
+ <!-- Categories 2 -->
+ 
+ <div class="row row-cols-1 row-cols-md-2">
+     
+     <!--Family-->           
+     <div class="col mb-4">
+         <div id ="family" class="card">
+         <a href="https://femurdesign.com/omni/">
+             <img src="../img/family.jpg" class="card-img-top" alt="family" style="width:100% height:auto">
+             <div class="card-body">
+                 <h5 class="card-title">For the family</h5>              
+             </div>  
+         </div>
+         </a>     
+     </div> 
+ 
+     <!--Individual-->
+     <div class="col mb-4">
+         <div id ="individual" class="card">
+         <a href="https://www.polkadotgame.com/">
+             <img src="../img/individual.jpg" class="card-img-top" alt="individual" style="width:100% height:auto">
+             <div class="card-body">
+                 <h5 class="card-title">For yourself</h5>              
+             </div>  
+         </div>
+         </a>     
+     </div> 
+ </div>
+ 
+ <!-- Categories 3 -->
+ 
+     <div class="row row-cols-1 row-cols-md-2">
+     
+         <!--easy to make-->         
+         <div class="col mb-4">
+             <div id ="easy" class="card">
+             <a href="http://wowenwilsonquiz.com/">
+                 <img src="../img/easy.jpg" class="card-img-top" alt="easy" style="width:100% height:auto">
+                 <div class="card-body">
+                     <h5 class="card-title">Easy to make</h5>              
+                 </div>  
+             </div>
+             </a>     
+         </div> 
+     
+         <!--hard to make-->
+         <div class="col mb-4">
+             <div id ="hard" class="card">
+             <a href="https://cat-bounce.com/">
+                 <img src="../img/hard.jpg" class="card-img-top" alt="hard" style="width:100% height:auto">
+                 <div class="card-body">
+                     <h5 class="card-title">Hard to make</h5>              
+                 </div>  
+             </div>
+             </a>     
+         </div> 
+                         
      </div>
-     </a>     
- </div> 
 
- <!--Individual-->
- <div class="col mb-4">
-     <div id ="individual" class="card">
-     <a href="https://www.polkadotgame.com/">
-         <img src="../img/individual.jpg" class="card-img-top" alt="individual" style="width:100% height:auto">
-         <div class="card-body">
-             <h5 class="card-title">For yourself</h5>              
-         </div>  
-     </div>
-     </a>     
- </div> 
+ </div>
 
-<!-- Categories 3 -->
-
-<div class="row row-cols-1 row-cols-md-2">
-
- <!--easy to make-->         
- <div class="col mb-4">
-     <div id ="easy" class="card">
-     <a href="http://wowenwilsonquiz.com/">
-         <img src="../img/easy.jpg" class="card-img-top" alt="easy" style="width:100% height:auto">
-         <div class="card-body">
-             <h5 class="card-title">Easy to make</h5>              
-         </div>  
-     </div>
-     </a>     
- </div> 
-
- <!--hard to make-->
- <div class="col mb-4">
-     <div id ="hard" class="card">
-     <a href="https://cat-bounce.com/">
-         <img src="../img/hard.jpg" class="card-img-top" alt="hard" style="width:100% height:auto">
-         <div class="card-body">
-             <h5 class="card-title">Hard to make</h5>              
-         </div>  
-     </div>
-     </a>     
- </div> 
-                 
 </div>
 
 '

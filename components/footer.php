5e180ea14316ca50d184dce081b7a201d3ef052d
sofiaGonzaloUtch <71847414+sofiaGonzaloUtch@users.noreply.github.com>
2020-11-16 21:46:17 -0700

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -2,17 +2,12 @@
 
 echo '
 
-<footer class="site-footer">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-8 col-sm-6 col-xs-12">
-            <p class="copyright-text">Copyright &copy <script>document.write(new Date().getFullYear());</script> All Rights Reserved by 
-         <a href="#">Cookie</a>.
-            </p>
-          </div>
-        </div>
-      </div>
-</footer>
+  <footer class="footer">
+    <div class="container">
+      <span class="text-muted">Copyright &copy <script>document.write(new Date().getFullYear());</script> All Rights Reserved by 
+      <a href="#">Cookie</span>
+    </div>
+  </footer>
 
 </body>
 

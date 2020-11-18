8465a367b29e532c6f869bd1fd31cc93121676bd
sofiaGonzaloUtch <71847414+sofiaGonzaloUtch@users.noreply.github.com>
2020-11-18 11:55:57 -0700

    Update categories.php

--- components/categories.php
+++ components/categories.php
@@ -1,39 +1,39 @@
 <?php
  echo'
 
- <div class=container mb-2>
- <br> <h1 id="rn" class="display-3">Welcome to Cookie!</h1>
- <p>Choose the category that best suits you</p> <br>
- </div>
+<div class=container mb-2>
+<br> <h1 id="rn" class="display-3">Welcome to Cookie!</h1>
+<p>Choose the category that best suits you</p> <br>
+</div>
  
 <div class = "container col-10">
  
- <!-- Categories 1 -->
+<!-- Categories 1 -->
  
- <div class="row row-cols-1 row-cols-md-2">
+<div class="row row-cols-1 row-cols-md-2">
  
          <!--Foods-->        
          <div class="col mb-4">
-             <div id ="foods" class="card">
-             <a href="https://www.thispersondoesnotexist.com">
-                 <img src="../img/food.jpg" class="card-img-top" alt="Foods" style="width:100% height:auto">
-                 <div class="card-body">
-                     <h5 class="card-title">Foods</h5>              
-                 </div>  
+             <div id ="foods">
+                 <a href="https://www.thispersondoesnotexist.com">
+                     <img src="../img/food.jpg" class="card-img-top" alt="Foods" style="width:100% height:auto">              
+                         <button type="button" class="btn btn-outline-dark col mb-4">Foods</button>                                 
+                 </a>  
              </div>
-             </a>     
+              
          </div> 
      
          <!--Desserts-->
          <div class="col mb-4">
-             <div id ="desserts" class="card">
-             <a href="https://www.windows93.net/#!catex">
-                 <img src="../img/dessert.jpg" class="card-img-top" alt="dessert" style="width:100% height:auto">
-                 <div class="card-body">
-                     <h5 class="card-title">Desserts</h5>              
-                 </div>  
+             <div id ="desserts">
+                 <a href="https://www.windows93.net/#!catex">
+                     <img src="../img/dessert.jpg" class="card-img-top" alt="dessert" style="width:100% height:auto">
+                     <div>
+                         <button type="button" class="btn btn-outline-dark col mb-4">Desserts</button>                 
+                     </div>  
+                 </a> 
              </div>
-             </a>     
+                
          </div> 
                      
  </div>
@@ -44,27 +44,30 @@
      
      <!--Family-->           
      <div class="col mb-4">
-         <div id ="family" class="card">
-         <a href="https://femurdesign.com/omni/">
-             <img src="../img/family.jpg" class="card-img-top" alt="family" style="width:100% height:auto">
-             <div class="card-body">
-                 <h5 class="card-title">For the family</h5>              
-             </div>  
+         <div id ="family">
+             <a href="https://femurdesign.com/omni/">
+                 <img src="../img/family.jpg" class="card-img-top" alt="family" style="width:100% height:auto">
+                 <div>
+                 <button type="button" class="btn btn-outline-dark col mb-4">For the family</button>               
+                 </div>  
+             </a>   
          </div>
-         </a>     
+           
      </div> 
  
      <!--Individual-->
      <div class="col mb-4">
-         <div id ="individual" class="card">
-         <a href="https://www.polkadotgame.com/">
-             <img src="../img/individual.jpg" class="card-img-top" alt="individual" style="width:100% height:auto">
-             <div class="card-body">
-                 <h5 class="card-title">For yourself</h5>              
-             </div>  
+         <div id ="individual">
+             <a href="https://www.polkadotgame.com/">
+                 <img src="../img/individual.jpg" class="card-img-top" alt="individual" style="width:100% height:auto">
+                 <div >
+                 <button type="button" class="btn btn-outline-dark col mb-4">For yourself</button>              
+                 </div>  
+             </a>    
          </div>
-         </a>     
+           
      </div> 
+
  </div>
  
  <!-- Categories 3 -->
@@ -73,26 +76,28 @@
      
          <!--easy to make-->         
          <div class="col mb-4">
-             <div id ="easy" class="card">
-             <a href="http://wowenwilsonquiz.com/">
-                 <img src="../img/easy.jpg" class="card-img-top" alt="easy" style="width:100% height:auto">
-                 <div class="card-body">
-                     <h5 class="card-title">Easy to make</h5>              
-                 </div>  
+             <div id ="easy">
+                 <a href="http://wowenwilsonquiz.com/">
+                     <img src="../img/easy.jpg" class="card-img-top" alt="easy" style="width:100% height:auto">
+                     <div>
+                         <button type="button" class="btn btn-outline-dark col mb-4">Easy to make</button>                  
+                     </div>  
+                 </a>      
              </div>
-             </a>     
+             
          </div> 
      
          <!--hard to make-->
          <div class="col mb-4">
-             <div id ="hard" class="card">
-             <a href="https://cat-bounce.com/">
-                 <img src="../img/hard.jpg" class="card-img-top" alt="hard" style="width:100% height:auto">
-                 <div class="card-body">
-                     <h5 class="card-title">Hard to make</h5>              
-                 </div>  
+             <div id ="hard">
+                 <a href="https://cat-bounce.com/">
+                     <img src="../img/hard.jpg" class="card-img-top" alt="hard" style="width:100% height:auto">
+                     <div>
+                     <button type="button" class="btn btn-outline-dark col mb-4">Hard to make</button>                 
+                     </div>  
+                 </a>       
              </div>
-             </a>     
+              
          </div> 
                          
      </div>

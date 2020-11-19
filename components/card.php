c78e22e5521efc16d7480c8420d5801d065b70d7
sofiaGonzaloUtch <71847414+sofiaGonzaloUtch@users.noreply.github.com>
2020-11-19 14:08:04 -0700

    Update card.php

--- components/card.php
+++ components/card.php
@@ -1,8 +1,10 @@
 <?php
 
 echo '
-<br><br> 
-<div class = "container col-15">';
+<br>
+<div class = "container col-30">
+<h3 style="text-align:center">What do you want to eat today?</h3><br>
+';
 
 for($y = 0; $y < 4; $y++){
 
@@ -14,8 +16,8 @@ echo '<div class="row">';
         <div class="col-md">
             <div id ="foods">
                 <a href="https://www.thispersondoesnotexist.com">
-                    <img src="https://3.bp.blogspot.com/-K4aLEzZ4CQU/Ujef__jBXKI/AAAAAAAAB-Q/xgSKIxsvjo8/s1600/perrito-triste.jpg" class="card-img-top" alt="Foods" style="width:100% height:auto">              
-                        <button type="button" class="btn btn-outline-dark col mb-4">Perrito</button>                                 
+                    <img src="https://cdn-img.health.com/sites/default/files/styles/large_16_9/public/1518546298/twelve-week-spaghetti-meatball-salad.jpg?itok=pTN-uFgV" class="card-img-top" alt="Foods" style="width:100% height:auto">              
+                        <button type="button" class="btn btn-outline-dark col mb-4">Spaguetti and Meatballs</button>                                 
                 </a>  
             </div>
         </div>
